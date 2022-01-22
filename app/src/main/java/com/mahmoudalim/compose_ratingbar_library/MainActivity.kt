cf791f036a41a1e602d4552214c8a4fc7de360de
Mahmoud Hussein <mahmoud.hussein@gen-c.com>
2022-01-22 16:42:06 +0200

    edit jitpack publishing gradle setting, remove local module dependency from app gradle

--- app/src/main/java/com/mahmoudalim/compose_ratingbar_library/MainActivity.kt
+++ app/src/main/java/com/mahmoudalim/compose_ratingbar_library/MainActivity.kt
@@ -3,16 +3,9 @@ package com.mahmoudalim.compose_ratingbar_library
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.mahmoudalim.compose_rating_bar.RatingBarView
 import com.mahmoudalim.compose_ratingbar_library.ui.theme.ComposeRatingbarlibraryTheme
 
 class MainActivity : ComponentActivity() {
